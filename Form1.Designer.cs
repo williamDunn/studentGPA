d32523b2d97ce6a30d83de54f4dfc3504a335ab6
Will <37053782+williamDunn@users.noreply.github.com>
2020-10-16 20:48:20 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Assignment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtGPA = new System.Windows.Forms.TextBox();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnHonors = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblProgrammerName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "GPA";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(140, 56);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 22);
+            this.txtFirstName.TabIndex = 2;
+            // 
+            // txtGPA
+            // 
+            this.txtGPA.Location = new System.Drawing.Point(140, 98);
+            this.txtGPA.Name = "txtGPA";
+            this.txtGPA.Size = new System.Drawing.Size(100, 22);
+            this.txtGPA.TabIndex = 3;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(347, 56);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(93, 17);
+            this.lblDate.TabIndex = 4;
+            this.lblDate.Text = "Aug 22, 2017";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(46, 172);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 17);
+            this.lblMessage.TabIndex = 6;
+            // 
+            // btnHonors
+            // 
+            this.btnHonors.Location = new System.Drawing.Point(45, 295);
+            this.btnHonors.Name = "btnHonors";
+            this.btnHonors.Size = new System.Drawing.Size(75, 23);
+            this.btnHonors.TabIndex = 7;
+            this.btnHonors.Text = "Honors Student?";
+            this.btnHonors.UseVisualStyleBackColor = true;
+            this.btnHonors.Click += new System.EventHandler(this.btnHonors_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(166, 295);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblProgrammerName
+            // 
+            this.lblProgrammerName.AutoSize = true;
+            this.lblProgrammerName.Location = new System.Drawing.Point(324, 370);
+            this.lblProgrammerName.Name = "lblProgrammerName";
+            this.lblProgrammerName.Size = new System.Drawing.Size(30, 17);
+            this.lblProgrammerName.TabIndex = 9;
+            this.lblProgrammerName.Text = "Will";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 451);
+            this.Controls.Add(this.lblProgrammerName);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnHonors);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.txtGPA);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtGPA;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnHonors;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblProgrammerName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*********************************************************
+         * Executed when the form is first loaded
+         * Sets lblDate to the current date
+         */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblDate.Text = DateTime.Now.ToString("MMM dd, yyyy");
+        }
+
+
+        private void btnHonors_Click(object sender, EventArgs e)
+        {
+            double gpa;
+
+            try
+            {
+                gpa = Convert.ToSingle(txtGPA.Text);
+                lblMessage.Text = "";
+
+                if(gpa <= 0 || gpa > 4)
+                {
+                    MessageBox.Show("Please enter a valid GPA", "Invalid GPA",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (gpa >= 3.75) // Honors
+                {
+                    lblMessage.Text = "HONORS STUDENTS";
+                    MessageBox.Show("Congratulations " + txtFirstName.Text.ToUpper() +
+                        "!! You will graduate with honors.",
+                        "HONORS", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else // No Honors
+                {
+                    MessageBox.Show("Sorry - No honors for you - better luck next time",
+                        "No Honors");
+                }
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        //*****************************************************************************
+        //Exit the program
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
